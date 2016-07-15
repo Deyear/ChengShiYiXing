b0be45731cd796b779c6004c589bae4a1cc6b086
Deyear <1207457319@qq.com>
2016-07-15 23:18:10 +0800

    Initial Commit

---
+++ ChengShiYiXing/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ChengShiYiXing
+//
+//  Created by 牛牛 on 16/7/15.
+//  Copyright © 2016年 HanghuanTech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
